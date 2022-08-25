aa5290c22a67e271a01f4f25fe652531c9176c19
lio <licroning@163.com>
2022-08-25 13:32:34 +0800

    chore: add dependency

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
+<script setup>
+import { isDark, toggleDark } from './composables/dark'
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtWelcome class="dark:important-bg-#001e26" text-black dark:important-text-white />
+  <main class="fixed z-100 w100% bottom-0 px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+    <nav text-xl mt-6>
+      <button class="icon-btn mx-2 !outline-none" title="切换深色模式" @click="toggleDark()">
+        <div i="carbon-sun dark:carbon-moon" />
+      </button>
+
+      <a
+        class="icon-btn mx-2" rel="noreferrer" href="https://github.com/lio-zero/nuxt3-starter" target="_blank"
+        title="GitHub"
+      >
+        <div i-carbon-logo-github />
+      </a>
+    </nav>
+  </main>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,45 @@
 import { defineNuxtConfig } from 'nuxt'
+import Components from 'unplugin-vue-components/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// Nuxt 2 Migrate to Nuxt 3：https://v3.nuxtjs.org/migration/configuration
+// Nuxt Configuration Reference：https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   typescript: {
     shim: false,
+  },
+  modules: [
+    '@unocss/nuxt',
+    'unplugin-icons/nuxt',
+    '@vueuse/nuxt',
+  ],
+  alias: {
+  },
+  css: [
+    '/styles/main.css',
+  ],
+  unocss: {
+    icons: true,
+    attributify: true,
+    webFonts: true,
+    typography: true,
+    wind: true,
+    shortcuts: [
+      ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+      ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ],
+  },
+  vite: {
+    plugins: [
+      Components({
+        dts: 'types/components.d.ts',
+        resolvers: [
+          // 自动注册图标组件
+          IconsResolver({
+            componentPrefix: '',
+          }),
+        ],
+      }),
+    ],
   },
 })
